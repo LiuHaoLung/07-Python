f6467b9c0df3b4a92446ee9d3ae4e8266a2c604c
LiuHaoLung <louis17171717@gmail.com>
2019-11-04 15:19:25 +0800

    Pylint

---
+++ Pylint/classforpylint1.py
@@ -0,0 +1,6 @@
+a = 1
+b = 2
+print(a)
+print(B)
+# 錯誤訊息會寫：undefined variable 'B'
+# 會有不同的錯誤報告
---
+++ Pylint/classforpylint2.py
@@ -0,0 +1,16 @@
+'''
+A Very simple script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+
+# 如果用 subine 也要注意，是用 tab 還是 space
