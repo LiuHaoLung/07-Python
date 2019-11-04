a943454bcb5ec01be7d7532a439695fd4e39ad58
LiuHaoLung <louis17171717@gmail.com>
2019-11-04 15:11:26 +0800

    IfElifElse

---
+++ IfElifElse/classforifelifelse.py
@@ -0,0 +1,73 @@
+if True:
+	print('ITS TRUE!')
+
+if 3 > 2:
+    print('ITS TRUE!')
+
+hungry = True
+if hungry:
+    print('FEED ME!')
+
+hungry = False
+if hungry:
+    print('FEED ME!')
+
+hungry = True
+if hungry:
+    print('FEED ME!')
+else:
+    print("I'm not hungry")
+
+loc = 'Bank'
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+else:
+    print('I do not know much.')
+
+loc = 'Bank'
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Bank':
+    print('Money is cool!')
+elif loc == 'Store':
+    print('Welcome to the store!')
+else:
+    print('I do not know much.')
+
+loc = 'Auto Shop'
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Bank':
+    print('Money is cool!')
+elif loc == 'Store':
+    print('Welcome to the store!')
+else:
+    print('I do not know much.')
+
+loc = 'Store'
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Bank':
+    print('Money is cool!')
+elif loc == 'Store':
+    print('Welcome to the store!')
+else:
+    print('I do not know much.')
+
+loc = 'Game'
+if loc == 'Auto Shop':
+    print('Cars are cool!')
+elif loc == 'Bank':
+    print('Money is cool!')
+elif loc == 'Store':
+    print('Welcome to the store!')
+else:
+    print('I do not know much.')
+
+name = 'Sammy'
+if name == 'Frankie':
+    print('Hello Frankie!')
+elif name == 'Sammy':
+    print('Hello Sammy!')
+else:
+    print('What is your name?')
